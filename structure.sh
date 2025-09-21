995d7af78a1a1a710df109c686922e9a4ef5a0a3
SwastikIIIT <whirphool123@gmail.com>
2025-09-21 11:56:41 +0530

    Folder structure

---
+++ structure.sh
@@ -0,0 +1,18 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+
+touch research/trials.ipynb
+
+touch .env
+
+touch setup.py
+touch app.py
+
+touch requirements.txt
+
+
+echo 'Directory and files created successfully!!'
